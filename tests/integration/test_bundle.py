f9150ca33adbc033610e54f8d4adb8a382b88c8b
natalia <natalianowakowska@mail.com>
2023-03-28 12:25:06 +0200

    add test for additional external idp

--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -93,3 +93,48 @@ async def test_kratos_is_up(ops_test: OpsTest):
 
     resp = requests.get(health_check_url)
     assert resp.status_code == 200
+
+
+async def test_multiple_kratos_external_idp_integrators(ops_test: OpsTest):
+    """Deploy an additional external idp integrator charm and test the action."""
+    additional_idp_name = "generic-external-idp"
+    config = {
+        "client_id": "client_id",
+        "client_secret": "client_secret",
+        "provider": "generic",
+        "issuer_url": "http://example.com",
+    }
+
+    await ops_test.model.deploy(
+        entity_url="kratos-external-idp-integrator",
+        application_name=additional_idp_name,
+        channel="edge",
+        config=config,
+        series="jammy",
+    )
+    await ops_test.model.add_relation(
+        "generic-external-idp:kratos-external-idp", "kratos:kratos-external-idp"
+    )
+
+    await ops_test.model.wait_for_idle(
+        raise_on_blocked=False,
+        status="active",
+        timeout=360,
+    )
+    assert ops_test.model.applications[additional_idp_name].units[0].workload_status == "active"
+    assert ops_test.model.applications["kratos"].units[0].workload_status == "active"
+
+    # Check that the redirect_uri is returned by the action
+    get_redirect_uri_action = (
+        await ops_test.model.applications[additional_idp_name]
+        .units[0]
+        .run_action("get-redirect-uri")
+    )
+    get_redirect_uri_action_output = await ops_test.model.get_action_output(
+        action_uuid=get_redirect_uri_action.entity_id,
+        wait=120,
+    )
+
+    redirect_uri = get_redirect_uri_action_output["redirect-uri"]
+    assert redirect_uri
+    logger.info(f"Redirect_uri: {redirect_uri}")
